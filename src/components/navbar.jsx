843711c0f160848eee30987012bf79582937b94b
BPerry35 <81257934+BPerry35@users.noreply.github.com>
2021-06-04 20:06:04 -0400

    Add files via upload

---
+++ src/components/navbar.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+// Stateless Functional Component
+
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-dark bg-dark">
+      <div className="navbar-brand">
+        <i className="fa fa-shopping-cart fa-lg m-2" aria-hidden="true" />
+        <span className="badge badge-pill badge-success m-2" style={{ width: 50 }}>
+          {totalCounters}
+        </span>
+        Brandon's Items
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
